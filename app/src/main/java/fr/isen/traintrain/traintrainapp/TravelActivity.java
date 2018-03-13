d2ff45337592d1b1ab4d79c8fcbe7efe4f552895
Unknown <flo831609@orange.fr>
2018-03-13 09:51:21 +0100

    Mise a jour menu

--- app/src/main/java/fr/isen/traintrain/traintrainapp/TravelActivity.java
+++ app/src/main/java/fr/isen/traintrain/traintrainapp/TravelActivity.java
@@ -32,14 +32,7 @@ public class TravelActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
         progressDialog = new ProgressDialog(this);
         progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
